c2865a5aecc9fdf6fe5fb4973881183522044492
Sebastian Probst Eide <sebastian.probst.eide@gmail.com>
2012-05-21 15:10:03 +0100

    [SignpostDemo] Updated jitter UDP protocol
    Changed the UDP protocol to make it easier to code
    against for non Cocoa devices:
    
    The message format expected of the packages are:
    'HOSTNAME\r\nTIMESTAMP\r\nJITTER'
    
    The hostname is the id of the host. Same as is used
    in the initial handshake.
    
    The timestamp is the time in seconds since 1970.
    The value used in the cocoa clients is a NSTimeInterval.
    The following is from its documentation:
    
      NSTimeInterval is always specified in seconds;
      it yields sub-millisecond precision over a range
      of 10,000 years.
    
    The jitter is also a double specifying jitter in ms.
    For the sending party, the jitter should be the jitter
    the client sees the in the messages received by the other
    party. The server will reply with the jitter seen for
    the jitter UDP probes from that particular client.

--- SharedCode/SharedCode.m
+++ SharedCode/SharedCode.m
@@ -136,34 +136,45 @@
 // so that multiple jitter measurements can be done simultaneously.
 - (NSData *) jitterPayloadContainingJitter:(NSNumber *)jitter
 {
-  NSArray *keys = [NSArray arrayWithObjects:@"host", @"date", @"jitter", nil];
   NSDate * date = [NSDate date];
-  NSArray *objects = [NSArray arrayWithObjects:self.hostname, date, jitter, nil];
-  NSDictionary *jitterData = [NSDictionary dictionaryWithObjects:objects forKeys:keys];
-  return [NSKeyedArchiver archivedDataWithRootObject:jitterData];
+  NSTimeInterval time = [date timeIntervalSince1970];
+  NSString *hostname = self.hostname;
+  double jitterFloat = [jitter floatValue];
+  
+  /***
+   * The format of the message sent:
+   * "hostname\r\ntimeSince1970\r\njitterFloat"
+   **/
+  NSString *jitterPayload = [NSString stringWithFormat:@"%@\r\n%f\r\n%f\r\n", hostname, time, jitterFloat];
+  return [jitterPayload dataUsingEncoding:NSUTF8StringEncoding];
+}
+
++ (NSArray *)partsFromData:(NSData *)data
+{
+  NSString *stringData = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+  return [stringData componentsSeparatedByString:@"\r\n"];
 }
 
 + (double) msFromTimestampData:(NSData *)data 
 {
-  NSDictionary *jitterData = [NSKeyedUnarchiver unarchiveObjectWithData:data];
-  NSDate * restoredDate = (NSDate *) [jitterData objectForKey:@"date"];
-  return [restoredDate timeIntervalSinceNow] * -1000.0;
+  NSArray *parts = [self partsFromData:data];
+  NSTimeInterval timeInterval = [(NSString *)[parts objectAtIndex:1] doubleValue];
+  NSDate *date = [NSDate dateWithTimeIntervalSince1970:timeInterval];
+  return [date timeIntervalSinceNow] * -1000.0;
 }
 
 + (NSString *) hostFromData:(NSData *)data 
 {
-  NSDictionary *jitterData = [NSKeyedUnarchiver unarchiveObjectWithData:data];
-  NSString *host = (NSString *) [jitterData objectForKey:@"host"];
-  return host;
+  NSArray *parts = [self partsFromData:data];
+  return [parts objectAtIndex:0];
 }
 
 + (NSNumber *) hostJitterFromData:(NSData *)data 
 {
-  NSDictionary *jitterData = [NSKeyedUnarchiver unarchiveObjectWithData:data];
-  NSNumber *jitter = (NSNumber *) [jitterData objectForKey:@"jitter"];
-  return jitter;
+  NSArray *parts = [self partsFromData:data];
+  double jitter = [(NSString *)[parts objectAtIndex:2] doubleValue];
+  return [NSNumber numberWithFloat:jitter];
 }
-
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
