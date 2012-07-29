9fcd9398ea3c0c055595666155352c3e2963a838
crotsos <cr409@cl.cam.ac.uk>
2012-07-29 13:45:15 -0700

    fixing year in json timestamp

--- SignpostServerOCaml/stats_sender.ml
+++ SignpostServerOCaml/stats_sender.ml
@@ -12,7 +12,7 @@ with _ ->
 
 let get_current_time_str =
   let timestamp = Unix.gmtime(Unix.time()) in
-  let time_str = Printf.sprintf "%d-%d-%dT%d:%d:%dZ" timestamp.Unix.tm_year
+  let time_str = Printf.sprintf "%02d-%02d-%02dT%02d:%02d:%02dZ" (timestamp.Unix.tm_year - 100)
     timestamp.Unix.tm_mon timestamp.Unix.tm_mday timestamp.Unix.tm_hour
     timestamp.Unix.tm_min timestamp.Unix.tm_sec in
   time_str
