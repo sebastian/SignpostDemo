fb0c8d8bb95c44268c49c09b4df1c24bc2ea377f
Andrius Aucinas <andrius.aucinas@gmail.com>
2012-08-01 00:11:18 +0100

    fixed the dynamics of json elemnets added/removed/updated

--- SignpostDemoWeb/signpost-nettree.js
+++ SignpostDemoWeb/signpost-nettree.js
@@ -10,13 +10,20 @@ var tree = d3.layout.tree()
 var diagonal = d3.svg.diagonal()
     .projection(function(d) { return [d.y, d.x]; });
 
+var tunLine = d3.svg.line()
+    .x(function(d) { return d.x; })
+    .y(function(d) { return d.y; })
+    .interpolate("cardinal").tension(0.2);
+
+
+
 /**
  * Prepare document's DOM for SVG drawing
  */ 
 // Chrome 15 bug: <http://code.google.com/p/chromium/issues/detail?id=98951>
-var div = d3.select("body").insert("div", "h2").insert("div").attr("id", "chart");
+var mainDiv = d3.select("body").insert("div", "h2").insert("div").attr("id", "chart");
 
-var vis = div.append("svg:svg")
+var vis = mainDiv.append("svg:svg")
     .attr("width", w+100)
     .attr("height", h)
   .append("svg:g");
@@ -24,15 +31,25 @@ var vis = div.append("svg:svg")
 d3.json("signpost-sigcomm-setup.json", function(json) {
   updateStructure(root = json);
 });
-
 d3.json("signpost-sigcomm-connections.json", function(json) {
-  updateTunnels(json);
+  updateTunnels(root = json);
 });
+
+setInterval(function() { 
+  d3.json("signpost-sigcomm-setup.json", function(json) {
+    updateStructure(root = json);
+  });
+
+  d3.json("signpost-sigcomm-connections.json", function(json) {
+    updateTunnels(root = json);
+  });
+},1000);
+
 
 
 // Time-series of node connection statistics
 
-var timeSeries = div.append("div")
+var timeSeriesDiv = mainDiv.append("div")
   .attr("id", "time-series")
   .style("top", "0px")
   .style("left", (w+120) + "px")
@@ -40,14 +57,23 @@ var timeSeries = div.append("div")
   .style("height", h+"px")
   .style("position", "absolute");
 
+
+
 // Cubism context
 // TODO: adjust delays, steps and size to appropriate values
 var context = cubism.context()
   .serverDelay(1 * 1000)   // Allow 30 seconds collection delay
-  .step(1e4)                // 15 seconds per value
+  .step(1e4)                // 10 seconds per value
   .size(500);
 
 var cube = context.cube("http://ec2-107-20-107-204.compute-1.amazonaws.com:1081");
+
+timeSeriesAxis = timeSeriesDiv
+  .append("svg")
+  .attr("class", "axis")
+  .append("g")
+  .attr("transform", "translate(0, 250)")
+  .call(context.axis().orient("top")/*.tickSize(6).ticks(d3.time.minutes, 15)*/);
 
 var nodes = null;
 
@@ -55,10 +81,9 @@ function updateStructure(source) {
 
   // Compute the new tree layout.
   nodes = tree.nodes(root).reverse();
-  console.log(nodes)
   // Update the nodes…
   var node = vis.selectAll("g.node")
-      .data(nodes, function(d) { return d.id || (d.id = ++i); });
+      .data(nodes, function(d){ return d.name;} );
 
 	var nodeEnter = node.enter().append("svg:g")
     	.attr("class", "node")
@@ -84,7 +109,7 @@ function updateStructure(source) {
 
   // Transition nodes to their new position.
   nodeEnter.transition()
-    .duration(duration)
+    // .duration(duration)
 		.attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; })
     .style("opacity", 1)
     .select("circle")
@@ -93,67 +118,41 @@ function updateStructure(source) {
     .style("fill", "lightsteelblue");
       
   node.transition()
-    .duration(duration)
+    // .duration(duration)
     .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; })
     .style("opacity", 1);
     
 
 	node.exit().transition()
-    .duration(duration)
+    // .duration(duration)
     .attr("transform", function(d) { return "translate(" + source.y + "," + source.x + ")"; })
     .style("opacity", 1e-6)
     .remove();
-/*
-	var nodeTransition = node.transition()
-		.duration(duration);
   
-  nodeTransition.select("circle")
-      .attr("cx", function(d) { return d.y; })
-      .attr("cy", function(d) { return d.x; })
-      .style("fill", function(d) { return d._children ? "lightsteelblue" : "#fff"; });
-  
-  nodeTransition.select("text")
-      .attr("dx", function(d) { return d._children ? -8 : 8; })
-	  .attr("dy", 3)
-      .style("fill", function(d) { return d._children ? "lightsteelblue" : "#5babfc"; });
-
-  // Transition exiting nodes to the parent's new position.
-  var nodeExit = node.exit();
-  
-  nodeExit.select("circle").transition()
-      .duration(duration)
-      .attr("cx", function(d) { return source.y; })
-      .attr("cy", function(d) { return source.x; })
-      .remove();
-  
-  nodeExit.select("text").transition()
-      .duration(duration)
-      .remove();
-*/
   // Update the links…
   var link = vis.selectAll("path.link")
-      .data(tree.links(nodes), function(d) { return d.target.id; });
+      .data(tree.links(nodes), function(d) { return d.source.name+"-"+d.target.name;});
 
 
   // Enter any new links at the parent's previous position.
   link.enter().insert("svg:path", "g")
     .attr("class", "link")
     .attr("d", function(d) {
-      var o = {x: source.x0, y: source.y0};
+      var o = {x: source.x, y: source.y};
       return diagonal({source: o, target: o});
     })
     .transition()
-    .duration(duration)
+    // .duration(duration)
     .attr("d", diagonal);
 
   // Transition links to their new position.
   link.transition()
-    .duration(duration)
+    // .duration(duration)
     .attr("d", diagonal);
 
   // Transition exiting nodes to the parent's new position.
   link.exit().transition()
-    .duration(duration)
+    // .duration(duration)
     .attr("d", function(d) {
       var o = {x: source.x, y: source.y};
       return diagonal({source: o, target: o});
@@ -167,26 +166,6 @@ function updateStructure(source) {
   });
 
   
-  
-  // TODO: no random!
-  function random(name, mult) {
-    var value = 0,
-        values = [],
-        i = 0,
-        last;
-    return context.metric(function(start, stop, step, callback) {
-      start = +start, stop = +stop;
-      if (isNaN(last)) last = start;
-      while (last < stop) {
-        last += step;
-        value = Math.abs(mult*Math.max(-10, Math.min(10, value/mult + .8 * Math.random() - .4 + .2 * Math.cos(i += .2))));
-        values.push(value);
-      }
-      callback(null, values = values.slice((start - stop) / step));
-    }, name);
-  }
-  
-
   //connection stats for each node (can draw all together, as only
   //one possible node on the other side in the current setup)
 
@@ -210,72 +189,91 @@ function updateStructure(source) {
       }
       return minDX;
     });
-  console.log(minDX);
+
+  timeSeriesAxis.attr("transform", "translate(0, "+(minX-60)+")")
 
   // For all clients, add connection stats graph
-  nodeEnter.filter(function(d) {
-      return d.type == "client";
-    })
-    .each(function(node) {
-      timeSeries.append("div")
-      .attr("id", "time-series-"+ node.name)
-      .call(function(div) {
-          div.attr("style", "position: absolute; top: "+(node.x-minDX/3-30)+"px;")
-          div.append("div")
-            .attr("class", "axis")
-            .call(context.axis().orient("top")/*.tickSize(6).ticks(d3.time.minutes, 15)*/);
-
-        var metrics = ["bandwidth", "latency", "jitter"];
-
-        div.selectAll(".horizon")
-          .data(metrics)
-          .enter().append("div")
-            .attr("class", "horizon")
-            .call(context.horizon()
-                .height(20)        // Breaks the graph!
-                .title(function(d) { return d; })
-                .metric(function(d) { return cube.metric("median(stats("+d+").eq(node,'"+node.name+"'))"); })
-            );
+  var timeSeriesData = timeSeriesDiv.selectAll(".time-series-class")
+    .data(nodes.filter(function(d){ return d.type=="client"; }), 
+            function(d){ return "time-series-"+d.name;}
+         );
 
 
-        div.append("div")
-          .attr("class", "rule")
-          .call(context.rule());
-      });
+  var timeSeriesEnter = timeSeriesData.enter()
+    .append("div")
+    .attr("id", function(d){return "time-series-"+d.name;})
+    .attr("class", "time-series-class")
+    .attr("style", function(d){ 
+      return "position: absolute; top: "+(d.x-minDX/3)+"px";
     });
 
+  var metrics = ["bandwidth", "latency", "jitter"];
+  var horizon = context.horizon().height(20);
+
+  timeSeriesEnter
+    .selectAll(".horizon")
+    .data(metrics)
+    .enter()
+    .append("div")
+    .attr("class", "horizon")
+    .call(horizon
+      .title(function(d) { return d; })
+      .metric(function(d) { return cube.metric("median(stats("+d+").eq(node,'"+node.name+"'))"); })
+    );
+ 
+  
+ timeSeriesEnter
+    .append("div")
+    .attr("class", "rule")
+    .call(context.rule());
+
+  timeSeriesData.transition()
+    // .duration(duration)
+    .attr("style", function(d){ 
+      return "position: absolute; top: "+(d.x-minDX/3)+"px";
+    });
+
+
+  timeSeriesData
+    .exit()
+    .remove();
 
 }
 
 function updateTunnels(tunnels){
-  //TODO: 'nodes' contains position coodinates - collect the actual tunnel
+  //'nodes' contains position coodinates - collect the actual tunnel
   //data and draw links betweent the different positions
   //nodes[i].x, nodes[i].y, nodes[i].name
 
-  var line = d3.svg.line()
-    .x(function(d) { return d.x; })
-    .y(function(d) { return d.y; })
-    .interpolate("cardinal").tension(0.2);
+  tunnels.forEach(function (d){
+    d.start = nodes.filter(function(nd){ return nd.name == d.client; })[0];
+    d.end = nodes.filter(function(nd){ return nd.name == d.server; })[0];
+    d.tunnelLine = [
+      {y: d.start.x, x: d.start.y}, 
+      {y: (d.start.x+d.end.x)/2, x: (d.start.y+d.start.y)/2+70}, 
+      {y: d.end.x, x: d.end.y}
+    ];
+  });
 
-  vis.selectAll("tunnel")
-    .data(tunnels.map(function(d){
-      console.log(d);
-      start = nodes.filter(function(nd){ return nd.name == d.client; })[0];
-      end = nodes.filter(function(nd){ return nd.name == d.server; })[0];
-      tunnelLine = [
-                    {y: start.x, x: start.y}, 
-                    {y: (start.x+end.x)/2, x: (start.y+start.y)/2+70}, 
-                    {y: end.x, x: end.y}
-      ];
-      console.log(tunnelLine);
+  var tunnelData = vis.selectAll(".tunnel")
+    .data(tunnels, function(d) { return d.client+"-"+d.server+"-"+d.type;});
 
-      return {line:tunnelLine, type:d.type};
-    }
-    ))
+
+  tunnelData
     .enter()
     .append("svg:path")
-    .attr("class", function(d){ return "tunnel-"+d.type;} )
-    .attr("d", function(d){ return line(d.line);} );
+    .attr("class", function(d){ return "tunnel tunnel-"+d.type;} )
+    .attr("d", function(d){ return tunLine(d.tunnelLine);} );
+
+  tunnelData
+    .transition()
+    // .duration(duration)
+    .attr("d", function(d){ return tunLine(d.tunnelLine);});
+
+  tunnelData
+    .exit()
+    // .duration(duration)
+    .remove();
 
 }
 
